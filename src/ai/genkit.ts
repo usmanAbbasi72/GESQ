a16bcea0a525bbf16b7345ca7eaa0162659b52ae
Piyar ali Abbasi <piyaraliabbasi52@gmail.com>
2025-09-17 07:19:31 +0000

    Try fixing this error: `Build Error: Module not found: Package path ./pr

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,8 +1,6 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import {groq} from 'genkit/providers/groq';
 
 export const ai = genkit({
-  plugins: [googleAI(), groq({apiKey: process.env.GROQ_API_KEY})],
-  model: 'groq/llama3-8b-8192',
+  plugins: [googleAI()],
 });
