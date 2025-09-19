a131b2d2c2124910b871519b2404f3312f49b3a4
Piyar ali Abbasi <piyaraliabbasi52@gmail.com>
2025-09-19 03:19:39 +0000

    add member only admin so if any from user side they havn't access to add

--- src/app/api/add-pending-member/route.ts
+++ src/app/api/add-pending-member/route.ts
@@ -1,15 +1,21 @@
-import { NextResponse } from 'next/server';
-import { db } from '@/lib/firebase';
-import { ref, push, set } from 'firebase/database';
+import {NextResponse} from 'next/server';
+import {db} from '@/lib/firebase';
+import {ref, push, set} from 'firebase/database';
 
 export async function POST(request: Request) {
   try {
+    const authorization = request.headers.get('Authorization');
+
+    if (authorization !== `Bearer ${process.env.API_SECRET_KEY}`) {
+      return NextResponse.json({message: 'Unauthorized'}, {status: 401});
+    }
+
     const body = await request.json();
-    const { userName, fatherName, cnic, role, event } = body;
+    const {userName, fatherName, cnic, role, event} = body;
 
     // Basic validation
     if (!userName || !fatherName || !cnic || !role) {
-      return NextResponse.json({ message: 'Missing required fields' }, { status: 400 });
+      return NextResponse.json({message: 'Missing required fields'}, {status: 400});
     }
 
     const newPendingMemberData = {
@@ -24,13 +30,15 @@ export async function POST(request: Request) {
     const newMemberRef = push(pendingMembersRef);
     await set(newMemberRef, newPendingMemberData);
 
-    return NextResponse.json({ message: 'Pending member added successfully', id: newMemberRef.key }, { status: 201 });
-
+    return NextResponse.json(
+      {message: 'Pending member added successfully', id: newMemberRef.key},
+      {status: 201}
+    );
   } catch (error) {
     console.error('Error adding pending member:', error);
     if (error instanceof SyntaxError) {
-      return NextResponse.json({ message: 'Invalid JSON body' }, { status: 400 });
+      return NextResponse.json({message: 'Invalid JSON body'}, {status: 400});
     }
-    return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+    return NextResponse.json({message: 'Internal Server Error'}, {status: 500});
   }
 }
