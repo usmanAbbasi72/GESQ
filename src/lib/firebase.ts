0e5b233fdceca879441b4ceb5ad81c6b83179cab
Piyar ali Abbasi <piyaraliabbasi52@gmail.com>
2025-12-04 17:15:03 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,13 @@ import { getAuth, Auth } from "firebase/auth";
 
 // Your web app's Firebase configuration using environment variables
 const firebaseConfig: FirebaseOptions = {
-  apiKey: "your-api-key",
-  authDomain: "your-auth-domain",
-  projectId: "your-project-id",
-  storageBucket: "your-storage-bucket",
-  messagingSenderId: "your-messaging-sender-id",
-  appId: "your-app-id",
-  measurementId: "your-measurement-id"
+  "projectId": "studio-3766857156-b0e75",
+  "appId": "1:810566605670:web:2874eb227af6b8405a06a7",
+  "apiKey": "AIzaSyBOGlG8Yuc7OsRtMvfxAJzAhjmClOKDMXQ",
+  "authDomain": "studio-3766857156-b0e75.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "810566605670",
+  "storageBucket": "studio-3766857156-b0e75.appspot.com"
 };
 
 // Singleton instances
